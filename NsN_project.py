8d2e405b2f9c36c34d1eea2441b1d1b30367cf7f
Erin Braswell <erin.braswell@gmail.com>
2011-05-23 20:45:38 -0400

    added the code

---
+++ NsN_project.py
@@ -0,0 +1,26 @@
+requesters = {'Erin': 3, 'Mary': 5, 'Sue':1, 'Frank': 2}
+
+MAXIMUM_REQUESTS = 5
+
+def show_requesters():
+	for name, request in requesters.items():
+		print '{0:5} has requested {1:5d}'.format(name, request)
+
+def new_entry():
+	new_name = raw_input('Please enter your name: ')
+	new_request = int(raw_input('Enter a number of items you want: '))
+
+	if new_name in requesters:
+		total = requesters[new_name]+new_request
+		if total<=MAXIMUM_REQUESTS:
+			requesters[new_name] = total
+			show_requesters()
+		else:
+			print 'You already requested the maximum.'
+			show_requesters()
+	else:
+		requesters[new_name] = new_request
+		show_requesters()
+			
+
+new_entry()
